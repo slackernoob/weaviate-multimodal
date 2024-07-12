47cf88fbe8f96a82fa776e0932a83e2ab47d8037
MaxAng <maxang12@gmail.com>
2024-07-12 17:40:00 +0800

    Update endpoint with VLM integration

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Optional, Union
-import json
-import logging
-import uvicorn
 import requests
 import weaviate
 import weaviate.classes as wvc
@@ -20,10 +17,9 @@ class InsertRequest(BaseModel):
 class nearTextQueryRequest(BaseModel):
     collection_name: str
     query: str
+    image: Optional[str] = None
 
 app = FastAPI()
-
-# logger = logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
 response = {
     'id': 'cmpl-beadb6213adb46738fae279276bbf564',
@@ -67,59 +63,63 @@ def insert_object(request: InsertRequest):
     collection = client.collections.get(collection_name)
     objects = request.objects
 
-    response = {
-        'id': 'cmpl-beadb6213adb46738fae279276bbf564',
-        'object': 'chat.completion',
-        'created': 1720513536,
-        'model': 'llava',
-        'choices': [{
-            'index': 0,
-            'message': {
-                'role': 'assistant',
-                'content': 'This image shows a pie chart with categories for sales. The chart is labeled "Sales chart" and includes four categories: Toys, Furniture, Home Decor, and Electronics. Each category has a different color and a percentage of the total sales assigned to it. The percentages are shown as follows: Toys with 28%, Furniture with 14%, Home Decor with 34%, and Electronics with 15%. The colors of the categories are blue, orange, and yellow, and the chart is presented against a white background. ',
-                'tool_calls': []
-            },
-            'logprobs': None,
-            'finish_reason': 'stop',
-            'stop_reason': None
-        }],
-        'usage': {
-            'prompt_tokens': 1196,
-            'total_tokens': 1319,
-            'completion_tokens': 123
-        }
-    }    
-
-    # resp = requests.post(
-    #     "http://10.255.252.128:8001/v1/chat/completions",
-    #     json={
-    #         "model": "llava",
-    #         "messages": [
-    #             {
-    #                 "role": "user",
-    #                 "content": [
-    #                     {"type": "text", "text": "What's in this image?"},
-    #                     {
-    #                         "type": "image_url",
-    #                         "image_url": {
-    #                             # "url": "https://www.spotfire.com/content/dam/spotfire/images/graphics/inforgraphics/pie1.png"
-    #                             "url": "simple_images/sample_charts/chart1.png"
-    #                         },
-    #                     },
-    #                 ],
-    #             }
-    #         ],
-    #     },
-    # )
     
+    # response = {
+    #     'id': 'cmpl-beadb6213adb46738fae279276bbf564',
+    #     'object': 'chat.completion',
+    #     'created': 1720513536,
+    #     'model': 'llava',
+    #     'choices': [{
+    #         'index': 0,
+    #         'message': {
+    #             'role': 'assistant',
+    #             'content': 'This image shows a pie chart with categories for sales. The chart is labeled "Sales chart" and includes four categories: Toys, Furniture, Home Decor, and Electronics. Each category has a different color and a percentage of the total sales assigned to it. The percentages are shown as follows: Toys with 28%, Furniture with 14%, Home Decor with 34%, and Electronics with 15%. The colors of the categories are blue, orange, and yellow, and the chart is presented against a white background. ',
+    #             'tool_calls': []
+    #         },
+    #         'logprobs': None,
+    #         'finish_reason': 'stop',
+    #         'stop_reason': None
+    #     }],
+    #     'usage': {
+    #         'prompt_tokens': 1196,
+    #         'total_tokens': 1319,
+    #         'completion_tokens': 123
+    #     }
+    # }    
+
     with collection.batch.dynamic() as batch:
         for item in objects:
             weaviate_obj = {}
+            base64_image = item.image
+            # Captions from the VLM endpoint
+            vlm_response = requests.post(
+                "http://10.255.252.128:8001/v1/chat/completions",
+                json={
+                    "model": "llava",
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": [
+                                {"type": "text", "text": "What's in this image?"},
+                                {
+                                    "type": "image_url",
+                                    "image_url": {
+                                        # "url": "https://www.spotfire.com/content/dam/spotfire/images/graphics/inforgraphics/pie1.png"
+                                        "url": f"data:image/jpeg;base64,{base64_image}"
+                                    },
+                                },
+                            ],
+                        }
+                    ],
+                },
+            )
+            # print(vlm_response.json())
+            captions = vlm_response.json()['choices'][0]['message']['content']
 
-            weaviate_obj["text"] = response['choices'][0]['message']['content']
+            weaviate_obj["text"] = captions
 
             if item.name is not None:
-                weaviate_obj["name"] = item.name
+                weaviate_obj["name"] = item.image
             if item.image is not None:
                 weaviate_obj["image"] = item.image
             batch.add_object(properties=weaviate_obj)
@@ -129,73 +129,53 @@ def insert_object(request: InsertRequest):
 
 
 
-'''
-{'id': 'cmpl-beadb6213adb46738fae279276bbf564', 
-'object': 'chat.completion', 
-'created': 1720513536, 
-'model': 'llava', 
-'choices': [{'index': 0, 
-            'message': {'role': 'assistant', 
-                        'content': 'This image shows a pie chart with categories for sales. The chart is labeled "Sales chart" and includes four categories: Toys, Furniture, Home Decor, and Electronics. Each category has a different color and a percentage of the total sales assigned to it. The percentages are shown as follows: Toys with 28%, Furniture with 14%, Home Decor with 34%, and Electronics with 15%. The colors of the categories are blue, orange, and yellow, and the chart is presented against a white background. ', 
-                        'tool_calls': []
-                        }, 
-            'logprobs': None, 
-            'finish_reason': 'stop', 
-            'stop_reason': None
-            }], 
-'usage': {'prompt_tokens': 1196, 
-        'total_tokens': 1319, 
-        'completion_tokens': 123}}
-'''
-# doesnt really work, not sure why
-# @app.post("/near_text_query")
-# def near_text_query(request: nearTextQueryRequest):
+
+@app.post("/near_text_query")
+def near_text_query(request: nearTextQueryRequest):
+    client = weaviate.connect_to_local()
+    collection_name = request.collection_name
+    query = request.query
+
+    base64_image = request.image
+    # Captions from the VLM endpoint
+    vlm_response = requests.post(
+        "http://10.255.252.128:8001/v1/chat/completions",
+        json={
+            "model": "llava",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "What's in this image?"},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                # "url": "https://www.spotfire.com/content/dam/spotfire/images/graphics/inforgraphics/pie1.png"
+                                # "url": "simple_images/sample_charts/chart1.png",
+                                "url": f"data:image/jpeg;base64,{base64_image}",
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
+    )
+    captions = vlm_response.json()['choices'][0]['message']['content']
+    collection = client.collections.get(collection_name)
     
     
-#     client = weaviate.connect_to_local()
-#     collection_name = request.collection_name
-#     collection = client.collections.get(collection_name)
-#     print(request.query) 
-    # with collection.batch.dynamic() as batch:
-#     for x in range(len(source)):
-#     # for src_obj in source:
-#         src_obj = source[x]
-#         path = directory + src_obj
-#         poster_b64 = to_base64(path)
-#         weaviate_obj = {
-#             # "name": src_obj,
-#             "name": os.path.splitext(src_obj)[0],
-#             "image": poster_b64,  # Add the image in base64 encoding
-#             "text": contextual_info[x] # Optional text field for image data
-#         }
+    
+    response = collection.query.near_text(
+        query=query + captions,
+        distance=0.6,
+        return_metadata=wvc.query.MetadataQuery(distance=True),
+        limit=3
+    )
+    res = []
 
-#         # The model provider integration will automatically vectorize the object
-#         batch.add_object(
-#             properties=weaviate_obj,
-#         )
+    for o in response.objects:
+        res.append(o.properties)
+        # print(o.properties)
+        print(o.metadata)
 
-# with collection_2.batch.dynamic() as batch:
-#     # for src_obj in source:
-#     for x in range(len(source)):
-#         src_obj = source[x]
-#         path = directory + src_obj
-#         poster_b64 = to_base64(path)
-#         weaviate_obj = {
-#             # "name": src_obj,
-#             "name": os.path.splitext(src_obj)[0],
-#             "image": poster_b64,  # Add the image in base64 encoding
-#             "text": contextual_info[x] # Optional text field for image data
-#         }
-
-#         # The model provider integration will automatically vectorize the object
-#         batch.add_object(
-#             properties=weaviate_obj,
-#         )
-        # print(" ------------------- ")
-    # print(response)
-    # for obj in response.objects:
-    #     print(obj.properties)
-    #     print(obj.metadata)
-    # client.close()
-    # return response
-    # return {"status": "success"}
+    return res
--- test2.py
+++ test2.py
@@ -1,12 +1,37 @@
 import requests
 import json
+import base64
+import os
+from PIL import Image
+from io import BytesIO
+
+# Helper function to convert file to base64 representation
+def to_base64(url_or_path):
+    if url_or_path.startswith('http://') or url_or_path.startswith('https://'):
+        # Handle URL
+        image_response = requests.get(url_or_path)
+        content = image_response.content
+    elif os.path.exists(url_or_path):
+        # Handle local file path
+        with open(url_or_path, 'rb') as image_file:
+            content = image_file.read()
+    else:
+        raise ValueError("The provided string is neither a valid URL nor a local file path.")
+    
+    return base64.b64encode(content).decode("utf-8")
+
+
+directory = "./simple_images/orange-cat.jpg"
+base64_string = to_base64(directory)
+
 
 # URL of your FastAPI endpoint
 url = "http://localhost:8000/near_text_query"
  
 payload = {
     "collection_name" : "DemoCollection",
-    "query": "Cats"
+    "query": "",
+    "image": base64_string,
     }
 
 try:
@@ -14,10 +39,22 @@ try:
     headers = {'Content-Type': 'application/json'}
     response = requests.post(url, headers=headers, data=json.dumps(payload))
 
-    # Print the response from the server
     print(response.status_code)
-    for o in response.objects:
-        print(o.properties)
-        print(o.metadata)
+    response_data = response.json()
+
+    for result in response_data:
+        print("-----")
+        print(result['text'])
+        print("--")
+        # print(result['name']) # base64 image
+        
+        print("--")
+        # print(result['image'])
+        
+    encoded_image = response_data[0]['name']
+    image_data = base64.b64decode(encoded_image)
+    image = Image.open(BytesIO(image_data))
+    image.show()
+    
 except Exception as e:
     print(f"Error sending request: {e}")
